f80077ddab93d12dfa361041c90346b444194e28
Ole Einar Christoph <olechristoph2412@gmail.com>
2022-07-21 13:28:12 +0200

    Added JS File and linked to HTML Page

--- JavaScript_Basics.js
+++ JavaScript_Basics.js
@@ -1,26 +1,25 @@
-<!--JavaScript basics-->
+//JavaScript basics
 
-<!--Öffnet eine Notification auf der Website-->
-<script>
+//Öffnet eine Notification auf der Website
+function simpleAlert(){
     alert("Hello World");
-</script>
+}
 
-<!--Variablen in JavaScript-->
-<script>
+function simpleAlertWithVar(){
     let eingabe; //let und var können variablen initialisieren. Wird die Variable nicht global genutzt sollte let verwendet werden da die Variable somit nur innerhalb des Anweisungsblocks gültig ist
     eingabe = prompt("Bitte geben Sie ihren Namen ein"); //Öffnet ein Popup, in dem etwas abgefragt werden kann
     if(eingabe == "Ingo"){
         alert("Ingo hat sich angemeldet")
     }
     alert("Hallo" + " " + eingabe)
-</script>
+}
 
-<!--Strict Mode-->
-<script>
+//Strict Mode
+function strictMode(){
     "use strict"; //aktiviert den strengen Modus. Hilft bei der Verwaltung der Variablen
                  // strenger Modus erfordert eine sorgfaltigere Programmierung und erlaubt keine undeklarierten Variablen oder Anweisungen ohne Simikolon
     let name;
     name = prompt("Bitte geben Sie ihren Namen ein");
     let ausgabe = "Wow, " + name + " ist ein schöner Name";
     alert(ausgabe);
-</script>
+}
